599457e5c90dc9697956321c3df553efc82dadba
Houssem Rezgui <121032019+HoussemRezgui999@users.noreply.github.com>
2023-01-24 13:05:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,207 @@
+var clubs=[{
+    clubName: "Arsenal",
+    imgSrc: "./images/arsenal.png",
+    rank: "./cap/rankings.png",
+    byName:"The Gunners",
+    desc: "Arsenal Football Club, commonly referred to as Arsenal, is a professional football club based in Islington, London, England. Arsenal plays in the Premier League, the top flight of English football", 
+    
+},{
+    clubName:"Aston Villa",
+    imgSrc: "./images/aston villa.png",
+    rank: "./cap/rankings.png",
+    byName:"The Villans",
+    desc:"Aston Villa Football Club is a professional football club based in Aston, Birmingham, England. The club competes in the Premier League, the top tier of the English football league system. Founded in 1874, they have played at their home ground, Villa Park, since 1897."
+},{
+    clubName:"Bournemouth",
+    imgSrc:"./images/bournemouth.png",
+    rank: "./cap/rankings.png",
+    byName:"The Cherries",
+    desc:"AFC Bournemouth is a professional association football club based in Kings Park, Boscombe, a suburb of Bournemouth, Dorset, England. The club competes in the Premier League, the highest division of English club football. Formed in 1899 as Boscombe, the club adopted their current name in 1971"
+
+},{
+    clubName:"Brentford",
+    imgSrc:"./images/brentford.png",
+    rank: "./cap/rankings.png",
+    byName:"The Bees",
+    desc:"Brentford Football Club is a professional football club in Brentford, West London, England, which competes in the Premier League, the highest tier of English football, having gained promotion via the playoffs at the end of the 2020/21 Championship season."
+},{
+    clubName:"Brighton",
+    imgSrc:"./images/brighton.png",
+    rank: "./cap/rankings.png",
+    byName:"The Seagulls",
+    desc:"Brighton & Hove Albion Football Club, commonly referred to simply as Brighton, is an English professional football club based in the city of Brighton and Hove. They compete in the Premier League, the top tier of the English football league system."
+},{
+    clubName:"Chelsea",
+    imgSrc:"./images/Chelsea.png",
+    rank: "./cap/rankings.png",
+    byName:"The Blues",
+    desc:"Chelsea Football Club is an English professional football club based in Fulham, West London. Founded in 1905, they play their home games at Stamford Bridge. The club competes in the Premier League, the top division of English football. They won their first major honour, the League championship, in 1955."
+},{
+    clubName:"Crystal Palace",
+    imgSrc:"./images/Crystal Palace FC.png",
+    rank:"./cap/rankings.png",
+    byName:"The Eagles",
+    desc:"Crystal Palace Football Club is a professional football club based in Selhurst in the Borough of Croydon, South London, England, who compete in the Premier League, the highest level of English football."
+},{
+    clubName:"Everton",
+    imgSrc:"./images/everton FC.png",
+    rank: "./cap/rankings.png",
+    byName:"The Toffees",
+    desc:"Everton Football Club is an English professional association football club based in Liverpool that competes in the Premier League, the top tier of English football."
+},{
+    clubName:"Fulham",
+    imgSrc:"./images/fulham FC.png",
+    rank: "./cap/rankings.png",
+    byName:"The Cottagers",
+    desc:"Fulham Football Club is an English professional football club based in Fulham, London, which compete in the Premier League. They have played home games at Craven Cottage since 1896, other than a two-year period spent at Loftus Road whilst Craven Cottage underwent redevelopments that were completed in 2004."
+    
+},{
+    clubName:"Leeds United",
+    imgSrc:"./images/leeds united FC.png",
+    rank:"./cap/rankings.png",
+    byName:"The Whites",
+    desc:"Leeds United Football Club is a professional football club based in Leeds, West Yorkshire in England. The club competes in the Premier League, the highest level of the English football league system, and plays its home matches at Elland Road."
+},{
+    clubName:"Leicester city",
+    imgSrc:"./images/leicester city.png",
+    rank : "./cap/rankings.png",
+    byName:"The Foxes",
+    desc:"Leicester City Football Club is an English professional football club based in Leicester in the East Midlands of England. The club competes in the Premier League, the highest level of England's football league system, and plays its home matches at King Power Stadium."
+},{
+    clubName:"Liverpool",
+    imgSrc:"./images/liverpool.png",
+    rank: "./cap/rankings.png",
+    byName:"The Reds",
+    desc:"Liverpool Football Club is a professional football club based in Liverpool, England. The club competes in the Premier League, the top tier of English football. Founded in 1892, the club joined the Football League the following year and has played its home games at Anfield since its formation."
+},{
+    clubName:"Manchester City",
+    imgSrc:"./images/manchester city.png",
+    rank: "./cap/rankings.png",
+    byName:"The Citizens",
+    desc:"Manchester City Football Club are an English football club based in Manchester that competes in the Premier League, the top flight of English football. Founded in 1880 as St. Mark's, they became Ardwick Association Football Club in 1887 and Manchester City in 1894."
+},{
+    clubName:"Manchester United",
+    imgSrc:"./images/manchester united.png",
+    rank : "./cap/rankings.png",
+    byName:"The Devils",
+    desc:"Manchester United Football Club, commonly referred to as Man United, or simply United, is a professional football club based in Old Trafford, Greater Manchester, England. The club competes in the Premier League, the top division in the English football league system."
+},{
+    clubName:"Nottingham Forest",
+    imgSrc:"./images/nottingham forest.png",
+    rank: "./cap/rankings.png",
+    byName:"The Tricky Trees",
+    desc:"Nottingham Forest Football Club is an association football club based in West Bridgford, Nottinghamshire, England. The club was founded in 1865 and have played their home games at the City Ground, on the banks of the River Trent, since 1898."
+},{
+    clubName:"Southampton",
+    imgSrc:"./images/southampton FC.png",
+    rank: "./cap/rankings.png",
+    byName:"The Saints",
+    desc:"Southampton Football Club is an English professional football club based in Southampton, Hampshire, which competes in the Premier League. Their home ground since 2001 has been St Mary's Stadium, before which they were based at The Dell. The club play in red and white shirts."
+},{
+    clubName:"Tottenham Hotspur",
+    imgSrc:"./images/tottenham hotspur.png",
+    rank: "./cap/rankings.png",
+    byName:"The Spurs",
+    desc:"Tottenham Hotspur Football Club, commonly referred to as Tottenham or Spurs, is a professional football club based in Tottenham, London, England. It competes in the Premier League, the top flight of English football."
+},{
+    clubName:"West Ham United",
+    imgSrc:"./images/west ham united.png",
+    rank: "./cap/rankings.png",
+    byName:"The Hammers",
+    desc:"West Ham United Football Club is an English professional football club that plays its home matches in Stratford, East London. The club competes in the Premier League, the top tier of English football. The club plays at the London Stadium, having moved from their former home, the Boleyn Ground, in 2016."
+},{
+    clubName:"Wolverhampton Wanderers",
+    imgSrc:"./images/wolverhampton wanderers.png",
+    rank: "./cap/rankings.png",
+    byName:"Wolves",
+    desc:"Wolverhampton Wanderers Football Club, commonly known as Wolves, is a professional football club based in Wolverhampton, England, which compete in the Premier League. The club has played at Molineux Stadium since moving from Dudley Road in 1889."
+},{
+    clubName:"Newcastle United",
+    imgSrc: "./images/newcastle.png",
+    rank:"./cap/rankings.png",
+    byName:"The Magpies",
+    desc:"Newcastle United Football Club is an English professional football club based in Newcastle upon Tyne, Tyne and Wear. The club competes in the Premier League, the highest level of the English football league system. The club was founded in 1892 by the merger of Newcastle East End and Newcastle West End." 
+}
+
+    
+]
+
+var each = function (coll,func){
+    if (Array.isArray(coll)){
+        for (var i =0;i<coll.length;i++){
+        func(coll[i],i)
+    }
+    }
+    else {
+        for (var key in coll){
+            func(coll[key],key)
+        }
+    }
+}
+
+var filter = function(array,predicate){
+    var acc = [];
+    each(array,function(e,i){
+        if (predicate(e,i)){
+            acc.push(e)
+        }
+    })
+    return acc;
+    }
+//////////////////////////////////////////////////////////////////////////
+
+each(clubs,function(e,i){
+    console.log(e)
+    $("#container").append('<div class="grid-item"><img id ="xcx"src="'+e.imgSrc+'"/></div>')
+    
+
+})
+
+$(".searchBtn").on("click",function(e){
+    var filt=filter(clubs,function(e,i){
+        var term=$(".searchOption").val()
+       return e.clubName.toLowerCase()===term.toLowerCase() || e.byName.toLowerCase()===term.toLowerCase()
+   
+
+       
+    })
+    $("#container").empty()
+    $(".searchOption").val("")
+    each(filt,function(e,i){
+        var div=$('<div " class="grid"><img id="img2" src="'+e.rank+'" /></div>')
+        var p=$("<p class='text'>'"+e.desc+"'</p>")
+        var img=$('<div  class="grid-item"><img class="img1" src="'+e.imgSrc+'"/></div>')
+        var h1=$("<h1>'"+e.byName+"'</h1><br>")
+        $("#container").append(h1).append(img).append(p).append(div)
+        })
+        
+        })
+
+        $(".btn").on("click",function home(e){
+            $("#container").empty()
+            each(clubs,function(e,i){
+                $("#container").append('<div class="grid-item"><img  id ="xcx"src="'+e.imgSrc+'"/></div>')
+
+            })
+        }
+        )
+
+
+
+
+       
+        
+
+
+      
+
+
+
+
+        
+            
+            
+        
+
+
+
